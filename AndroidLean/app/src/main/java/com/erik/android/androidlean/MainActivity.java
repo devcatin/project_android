b9c757b6f9863c40d3f416cb3f3aeb6018357e06
devcatin <605435554@qq.com>
2019-05-14 13:10:39 +0800

    no message

--- AndroidLean/app/src/main/java/com/erik/android/androidlean/MainActivity.java
+++ AndroidLean/app/src/main/java/com/erik/android/androidlean/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.erik.utilslibrary.UtilsTools;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("key", "Hello world!");
+        int sum = UtilsTools.add(100, 120);
     }
 }
---
+++ AndroidLean/utilslibrary/src/main/java/com/erik/utilslibrary/UtilsTools.java
@@ -0,0 +1,8 @@
+package com.erik.utilslibrary;
+
+public class UtilsTools {
+
+    static public int add(int x, int y) {
+        return x + y;
+    }
+}
