cfa611a1fce0eb2737603042a8896e0421e297b0
devcatin <605435554@qq.com>
2019-05-15 18:43:49 +0800

    全局参数参数的抽取

--- AndroidLean/app/src/main/java/com/erik/android/androidlean/MainActivity.java
+++ AndroidLean/app/src/main/java/com/erik/android/androidlean/MainActivity.java
@@ -1,11 +1,20 @@
 package com.erik.android.androidlean;
 
+import android.app.ActionBar;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.erik.utilslibrary.UtilsTools;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("key", "Hello world!");
         int sum = UtilsTools.add(100, 120);
+
+        TextView textView = new TextView(this);
+        textView.setText("Hello world!");
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        super.setContentView(linearLayout);
+
+        final TextView show = new TextView(this);
+
+        Button button = new Button(this);
+        button.setText("ok");
+        button.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(show);
+        linearLayout.addView(button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show.setText("Hello, Android," + new java.util.Date());
+            }
+        });
     }
 }
