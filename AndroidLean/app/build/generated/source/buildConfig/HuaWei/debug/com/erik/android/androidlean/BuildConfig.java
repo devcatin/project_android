36507145db0e0249d942c190702401eb1d0af166
devcatin <605435554@qq.com>
2019-06-24 19:01:01 +0800

    添加签名多渠道打包

---
+++ AndroidLean/app/build/generated/source/buildConfig/HuaWei/debug/com/erik/android/androidlean/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.erik.android.androidlean;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.erik.android.androidlean";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "HuaWei";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from product flavor: HuaWei
+  public static final boolean ENABLE_LOG = true;
+  public static final int Environment = 1;
+}
